3a74a15155139e6341d9a6cb1ddc60a2412869dd
Francisco Fontes <fontes.fj@gmail.com>
2021-09-23 10:38:39 -0300

    Commit inicial

---
+++ src/test/java/com/zallpy/appointment/AppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zallpy.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
